f33700e5558092cfcfa8471eda63da5ef3184399
Adriel Medeiros <130292427+Athelstan0420@users.noreply.github.com>
2024-05-18 00:02:09 -0300

    Corrigindo o erro ortográfico.

--- Cods/Ex3.py
+++ Cods/Ex3.py
@@ -2,7 +2,7 @@
 #Somando números:
 
 num1 = int(input("Digite o primeiro valor: "))
-num2 = int(input("dIGITE O= o segundo valor: "))
+num2 = int(input("Digite o segundo valor: "))
 soma = num1 + num2
 
 print(f"A soma é: {num1} + {num2} = {soma}")3
