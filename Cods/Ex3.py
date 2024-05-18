606b2d42e73e300bd374b4cb0b99eeedca6d4309
Adriel Medeiros <adriel.medeiros@academico.ifpb.edu.br>
2024-05-17 22:43:11 -0300

    Create Ex3.py

---
+++ Cods/Ex3.py
@@ -0,0 +1,8 @@
+
+#Somando números:
+
+num1 = int(input("Digite o primeiro valor: "))
+num2 = int(input("dIGITE O= o segundo valor: "))
+soma = num1 + num2
+
+print(f"A soma é: {num1} + {num2} = {soma}")3
