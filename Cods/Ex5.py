60194d660942002c2f13b19246b4cdea086567b5
Adriel Medeiros <130292427+Athelstan0420@users.noreply.github.com>
2024-05-20 22:28:57 -0300

    ex. feito pelo pystudio

---
+++ Cods/Ex5.py
@@ -0,0 +1,7 @@
+num_entrada = int(input("Digite um número: "))
+
+sucessor = num_entrada + 1
+
+antecessor = num_entrada - 1
+
+print("O número digitado foi {}, seu antecessor é {}, seu sucessor é {}".format(num_entrada,antecessor,sucessor))
