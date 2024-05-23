32a062a3bff8fce5a9353c4ff6bdb13601045eb8
Adriel Medeiros <130292427+Athelstan0420@users.noreply.github.com>
2024-05-23 00:10:25 -0300

    Atualizar o Ex11.py Corrigindo

--- Cods/Ex11.py
+++ Cods/Ex11.py
@@ -1,1 +1,16 @@
-.
+#Pintando uma Parede:
+
+while True:
+    try:
+        l = float(input("Largura da parede em m2: "))
+        a = float(input("Altura da parede em m2:"))
+        area = l * a        
+        tinta = area / 4              
+    except:
+        print("Ops! Eu erro")
+    else:
+        print("A área dessa parede {} x {} é = {:.2f}m2".
+        format(l,a,area))
+        print("A quantidade de litros necessários será de {:.2f} litros".format(tinta))
+    finally:
+        print("Continue Programando..")
