af61716f28e2a0307b385857ca8ddbd980036602
Adriel Medeiros <130292427+Athelstan0420@users.noreply.github.com>
2024-05-29 16:42:01 -0300

    Atualizar o Ex17.py

--- Cods/Ex17.py
+++ Cods/Ex17.py
@@ -5,7 +5,9 @@ from math import sqrt
 try: 
     cateto1 = int(input("Cateto 1: "))
     cateto2 = int(input("Cateto 2 : "))
-    raiz1 = cateto1*cateto1 + cateto2*cateto2
+    #Soma do quadrado de cada cateto
+    raiz1 = cateto1*cateto1 + cateto2*cateteto2
+    #Raiz da soma do quadrado de cada cateto
     raiz2 = sqrt(raiz1)    
 except:
     print("Ops")    
