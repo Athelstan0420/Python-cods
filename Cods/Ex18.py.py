fb92b1ed7ecfdabbe2f2274eabb0a116f8791ad6
Adriel Medeiros <adriel.medeiros@academico.ifpb.edu.br>
2024-06-03 18:10:15 -0300

    Create Ex18.py.py

---
+++ Cods/Ex18.py.py
@@ -0,0 +1,28 @@
+# Trigonometria: {
+
+# Razões Trigonométricas:
+
+# SENO, COSSENO E TANGENTE {
+#
+# Divisões realizadas entre as medidas de lados de um triângulo RETÂNGULO --> Polígono, com um ângulo reto,
+# Hipotenusa = Lado que se opõe ao ângulo reto. MAIOR LADO;
+
+# --> OPOSTO =  
+# --> ADJACENTE = 
+# 
+# --> Objetivo: Relacionar a media dos lados com a medida dos ângulos;
+# EXEMPLO DE TRIÂNGULO RETANGULO;
+# import webbrowser
+# url = "https://s2.static.brasilescola.uol.com.br/img/2017/12/exemplo-de-triangulo-retangulo(1).jpg"
+# img = webbrowser.open(url)
+# ---------------------------------------------------------------------------------------------------
+
+# Documentação math= https://docs.python.org/3/library/math.html
+import math
+ang = float(input("Digite o ângulo: "))
+print(f"{math.}")
+
+
+
+
+
