c78095bc50a9b29cf0df3f1b26d8902b5e5b35db
Adriel Medeiros <130292427+Athelstan0420@users.noreply.github.com>
2024-06-10 16:02:22 -0300

    Atualizar o Cods

---
+++ Cods/Ex19.py
@@ -0,0 +1,11 @@
+
+# Sorteando uma lista
+
+#Utiliza-se o "choice" quando se quer sortear uma lista de strings
+
+import random # biblioteca random
+
+nomes = ["Adriel", "Maria", "Erika"]
+sorteio = random.choice(nomes) 
+
+print(sorteio)
